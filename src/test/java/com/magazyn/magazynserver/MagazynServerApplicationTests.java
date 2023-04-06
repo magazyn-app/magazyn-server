37440c967dc8224e5a9c1d62e100a3163b81d627
Michał Cop <michalcop@bntech.dev>
2023-04-06 18:08:08 +0200

    Init repo

---
+++ src/test/java/com/magazyn/magazynserver/MagazynServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magazyn.magazynserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazynServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
